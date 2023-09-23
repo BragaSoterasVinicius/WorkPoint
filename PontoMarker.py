7ff2406fbe35aab7b0cbc1b3c8c34b71b0d72f0b
BragaSoterasVinicius <viniciussoteras@gmail.com>
2023-09-23 01:28:35 -0300

    Pensei em automatizar a marcação de ponto, no final de contas ficou tão simples que até a vontade de fazer algo melhor vai embora

---
+++ PontoMarker.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys 
+import login_info
+
+servico = Service(ChromeDriverManager().install())
+
+navegador = webdriver.Chrome(service = servico)
+
+navegador.get(login_info.website)
+
+navegador.find_element('xpath','//*[@id="txtLogin"]').send_keys(login_info.login)
+
+navegador.find_element('xpath','//*[@id="txtSenha"]').send_keys(login_info.send_keys)
+
+navegador.find_element('xpath','//*[@id="btnEntrar"]').click()
+
+# Como posso controlar a janela para marcação apropriada do ponto?
